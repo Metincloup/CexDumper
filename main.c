53361d73c8d79f56eca8ab2142740d7dff93ee9b
Metin <88655361+Metincloup@users.noreply.github.com>
2026-03-14 02:54:50 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define PROGRAMNAME "CexDumper"
+#define VERSION "v0.0.1"
+
+int main(int argc, char const *argv[])
+{
+    printf("%s %s - C Hex Dumper\n", PROGRAMNAME, VERSION);
+    return 0;
+}
